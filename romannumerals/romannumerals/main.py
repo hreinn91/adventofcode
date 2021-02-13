c8edaf00b300983b51c1889c91a3e1df588640cf
hreinn91 <hreinnj@localhost.localdomain>
2021-02-13 14:56:59 +0100

    Simple solution for most numbers

--- romannumerals/romannumerals/main.py
+++ romannumerals/romannumerals/main.py
@@ -1,18 +1,32 @@
+numeral_map = {1: 'I',
+               5: 'V',
+               10: 'X',
+               # 50: 'L',
+               # 100: 'C',
+               # 500: 'D',
+               # 1000: 'M'
+               }
 
-numerals = {1: 'I',
-            5: 'V',
-            10: 'X',
-            50: 'L',
-            100: 'C',
-            500: 'D',
-            1000: 'M'}
+mappable_number = sorted(numeral_map.keys(), reverse=True)
 
-def get_numeral(number):
 
+def convert_numeral_simple(number):
+    if number == 0:
+        return ''
+
+    converted_number = ''
+    for key in mappable_number:
+        while number >= key:
+            converted_number = converted_number + numeral_map[key]
+            number = number - key
+
+    return converted_number
+
+
+def convert_odd_numbers(number):
+    if number == key - 1:
+        return converted_number + 'I' + convert_numeral(key)
 
 
 def main():
-
-
     pass
-
--- romannumerals/test_numerals.py
+++ romannumerals/test_numerals.py
@@ -1,7 +1,20 @@
-from romannumerals.main import get_numeral
+from romannumerals.main import convert_numeral
 
 
 def test_simple_numeral():
-    assert get_numeral(1) == 'I'
-    assert get_numeral(2) == 'II'
-    assert get_numeral(3) == 'III'
+    assert convert_numeral_simple(1) == 'I'
+    assert convert_numeral_simple(2) == 'II'
+    assert convert_numeral_simple(3) == 'III'
+
+    # assert convert_numeral_simple(4) == 'IV'
+    assert convert_numeral_simple(5) == 'V'
+    assert convert_numeral_simple(6) == 'VI'
+    assert convert_numeral_simple(7) == 'VII'
+    assert convert_numeral_simple(8) == 'VIII'
+
+    # assert convert_numeral_simple(9) == 'IX'
+    assert convert_numeral_simple(10) == 'X'
+    assert convert_numeral_simple(11) == 'XI'
+    assert convert_numeral_simple(12) == 'XII'
+    assert convert_numeral_simple(13) == 'XIII'
+    # assert convert_numeral_simple(14) == 'XIV'
