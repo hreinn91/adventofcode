12e7983adab32e59408f4819849b28fb5a3d2d5e
hreinn91 <hreinnj@localhost.localdomain>
2021-07-25 16:33:47 +0200

    some bugg in the calc of shortest manhattan distance

--- year2019/day3/main.py
+++ year2019/day3/main.py
@@ -1,7 +1,22 @@
+def get_input(filename):
+    raw_split = open(filename, 'r').read().split('\n')
+    wire_1 = raw_split[0].split(',')
+    wire_2 = raw_split[1].split(',')
+    return wire_1, wire_2
+
+
+def norm_l1(x, y):
+    dx = abs(x)
+    dy = abs(y)
+    return dx + dy
+
+
 class Wires:
     def __init__(self, filename):
         self.wire_1, self.wire_2 = get_input(filename)
         self.grid = self.get_grid(self.wire_1, self.wire_2)
+        self.intersections = self.get_intersections(self.grid)
+        self.manhattan_distance = self.get_shortest_intersection(self.intersections)
 
     @staticmethod
     def get_grid(wire_1, wire_2):
@@ -64,9 +79,9 @@ class Wires:
 
     def print_grid(self, grid):
         x_size, y_size = self.get_grid_dimensions(grid)
-        for y in range(0, y_size + 2):
+        for y in range(-100, y_size + 2):
             row = []
-            for x in range(0, x_size + 2):
+            for x in range(-100, x_size + 2):
                 if (x, y) in grid:
                     row.insert(x, grid[(x, y)])
                 else:
@@ -74,18 +89,31 @@ class Wires:
             print(''.join(row))
         pass
 
-    def norm_l1(self, x, y):
-        dx = abs(x)
-        
+    @staticmethod
+    def get_intersections(grid):
+        intersections = {}
+        for key, value in grid.items():
+            if value == 'X':
+                intersections[key] = value
+        return intersections
 
+    @staticmethod
+    def get_shortest_intersection(intersections):
+        if len(intersections) == 0:
+            raise RuntimeError('Get distance error')
 
-def get_input(filename):
-    raw_split = open(filename, 'r').read().split('\n')
-    wire_1 = raw_split[0].split(',')
-    wire_2 = raw_split[1].split(',')
-    return wire_1, wire_2
+        first_key = list(intersections.keys())[0]
+        manhattan_distance = norm_l1(first_key[0], first_key[1])
+        for key in intersections.keys():
+            temp_distance = norm_l1(key[0], key[1])
+            if temp_distance < manhattan_distance:
+                manhattan_distance = temp_distance
+        return manhattan_distance
 
 
 if __name__ == '__main__':
-    wires = Wires('test_input1.txt')
+    wires = Wires('input1.txt')
+    wires.print_grid(wires.grid)
+    print(wires.manhattan_distance)
+
     print("Done")
--- year2019/day3/test.py
+++ year2019/day3/test.py
@@ -1,25 +1,37 @@
 import unittest
 from main import Wires
+from main import norm_l1
 
-test_wires = Wires('test_input1.txt')
+wires = Wires('test_input1.txt')
 
 
 class MainTestCase(unittest.TestCase):
     def test_parse(self):
-        wire_1 = test_wires.wire_1
-        direction, length = test_wires.parse(wire_1[0])
+        wire_1 = wires.wire_1
+        direction, length = wires.parse(wire_1[0])
         self.assertEqual(direction, 'R')
         self.assertEqual(length, 8)
 
+    def test_norm_l1(self):
+        self.assertEqual(norm_l1(3, 3), 6)
+        self.assertEqual(norm_l1(-3, 3), 6)
+        self.assertEqual(norm_l1(2, 9), 11)
+
     def test_get_grid(self):
-        grid = test_wires.grid
+        grid = wires.grid
+        wires.print_grid(grid)
         self.assertEqual(len(grid), 41)
 
-        dim = test_wires.get_grid_dimensions(grid)
+        dim = wires.get_grid_dimensions(grid)
         self.assertEqual(dim[0], 8)
         self.assertEqual(dim[1], 7)
 
-        test_wires.print_grid(grid)
+        intersections = wires.intersections
+        self.assertEqual(len(intersections), 2)
+
+        manhattan_distance = wires.get_shortest_intersection(intersections)
+        self.assertEqual(manhattan_distance, 6)
+
 
 
 if __name__ == '__main__':
